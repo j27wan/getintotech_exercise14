59cba02f83e483a4adac03483164dc1650b067fa
Ellen Houghton <ellen.a.ash@gmail.com>
2023-02-17 12:15:49 +0000

    add score tracker function and broke game logic

--- extra_functions.py
+++ extra_functions.py
@@ -4,3 +4,13 @@ import sys
 def end_game():
     print("Goodbye!")
     sys.exit()
+
+
+def score_tracker(winner, player_score, computer_score):
+    if winner == "player":
+        player_score +=1
+    elif winner == "computer":
+        computer_score +=1
+    else:
+        print("it is a draw")
+    print(f"Player Score: {player_score}\nComputer Score: {computer_score}")
--- game.py
+++ game.py
@@ -23,26 +23,37 @@ def play_game():
     choice_dict = {'R': 'Rock', 'P': 'Paper', 'S': 'Scissors'}
     print(f'You have chosen: {choice_dict[user_input]} \nThe computer has chosen: {choice_dict[computer_input]}')
 
+    winner = ""
+
     if user_input == computer_input:
         print("You have drawn.")
-
+        winner = "None"
+        
     elif user_input == 'R':
         if computer_input == 'P':
             print("You have lost. Computer wins.")
+            winner = "computer"
         else:
             print("You have won. Player wins.")
+            winner = "player"
 
     elif user_input == 'P':
         if computer_input == 'S':
             print("You have lost. Computer wins.")
+            winner = "computer"
         else:
             print("You have won. Player wins.")
+            winner = "player"
 
     else:
         if computer_input == 'R':
             print("You have lost. Computer wins.")
+            winner = "computer"
         else:
             print("You have won. Player wins.")
+            winner = "player"
+
+    extra_functions.score_tracker(winner, player_score, computer_score)
 
     play_again()
 
@@ -62,5 +73,6 @@ def play_again():
     else:
         print("Not recognised. Please type Y or N.")
 
-
+player_score = 0
+computer_score = 0
 play_game()
