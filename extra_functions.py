b6a5e4dd5e5bb806f2bbcd6ceb0f91dcbd852831
Jessica Wan <j27wan@gmail.com>
2023-02-18 12:14:43 +0000

    Comments added for readability

--- extra_functions.py
+++ extra_functions.py
@@ -1,17 +1,27 @@
 import sys
 
 
+# exit out of game using exit() function from imported sys library
 def end_game():
     print("Goodbye!")
     sys.exit()
 
 
+# function to track score - takes in parameters of variables winner, player_score and computer_score
+# initially, the score variables have a value of 0 until updated throughout
 def score_tracker(winner, player_score, computer_score):
+
+    # conditional statements which check winner variable by matching string using equality operator
+    # += is incrementing assignment operator and adds 1 to our value
     if winner == "player":
-        player_score +=1
+        player_score += 1
+
     elif winner == "computer":
-        computer_score +=1
+        computer_score += 1
+
     else:
-        print("it is a draw")
+        print("You have drawn with the Computer.")
     print(f"Player Score: {player_score}\nComputer Score: {computer_score}")
+
     return player_score, computer_score
+    # return the player_score and computer_score values as a tuple on line 79 under score_tuple
--- game.py
+++ game.py
@@ -1,40 +1,52 @@
 import random
 import extra_functions
+# importing random for computer choice
+# importing extra_functions for our code
 
 
+# code is called at end of page after def functions using play_game()
 def user_choice():
+    # declaring a variable using empty string
+    choice = ""
 
-    choice = ''
-    while choice not in ['R', 'P', 'S', 'r', 'p', 's', 'Q', 'q']:
+    # while loop used to repeatedly accept input until user provides input within defined list
+    # not sure how many times it will take for user to input acceptable response
+    while choice not in ['R', 'P', 'S', 'Q']:
         choice = input('(R)ock, (P)aper, (S)cissors or (Q)uit?').upper()
+    # .upper is a method - capitalises the choice if user does not input the exact string
 
+    # offer user an option to break out of while loop and quit using Q/q
+    # end_game() is a function which is called from extra_functions file; terminates the program
     if choice == 'Q':
         print("Thank you, goodbye.")
         extra_functions.end_game()
 
-        # else choice == 'R' or choice == 'P' or choice == 'S':
     else:
         return choice
-    
+
+        # original code was if, elif and else statements only using multiple choices as shown below
+        # if choice == 'R' or choice == 'P' or choice == 'S':
         # elif choice == 'r' or choice == 'p' or choice == 's':
         #     return choice.upper()
-        #
         # else:
         # print("You have to enter a letter, try again")
         # user_choice()
 
 
 def play_game(player_score, computer_score):
+    # user_input is assigned from the returned choice from user_choice() function as defined above
     user_input = user_choice()
+    # computer_input is assigned using the choice() function from imported random library
     computer_input = random.choice(["R", "P", "S"])
 
+    # dictionary used to convert input characters into words for our string
+    # key is the letter, value (object?) is the associated word
     choice_dict = {'R': 'Rock', 'P': 'Paper', 'S': 'Scissors'}
     print(f'You have chosen: {choice_dict[user_input]} \nThe computer has chosen: {choice_dict[computer_input]}')
 
-    winner = ""
-
+    # core game logic using conditional statements to determine the winner
+    # assigns string to winner variable depending on the combination
     if user_input == computer_input:
-        print("You have drawn.")
         winner = "None"
         
     elif user_input == 'R':
@@ -61,11 +73,19 @@ def play_game(player_score, computer_score):
             print("You have won. Player wins.")
             winner = "player"
 
-    tuple = extra_functions.score_tracker(winner, player_score, computer_score)
-    (new_player_score, new_computer_score) = tuple
+    # call score_tracker() function from extra_functions file
+    # provide parameters of variables of winner, player_score and computer_score
+    # this returns a tuple containing 2 values which indicate updated scores
+    score_tuple = extra_functions.score_tracker(winner, player_score, computer_score)
+
+    # unpack tuple by assigning updated scores to new variables
+    (new_player_score, new_computer_score) = score_tuple
+
+    # call play_again() function with updated scores as parameters
     play_again(new_player_score, new_computer_score)
 
 
+# function requiring user input to offer choice to play again or terminate the program
 def play_again(new_player_score, new_computer_score):
     replay = input("Do you want to play again? Press Q to quit or enter to play again.")
 
@@ -76,6 +96,10 @@ def play_again(new_player_score, new_computer_score):
         play_game(new_player_score, new_computer_score)
 
 
+# our initial declarations of variables are shown below - now removed from code
 # player_score = 0
 # computer_score = 0
-play_game(player_score = 0, computer_score = 0)
+
+# now using 2 positionally based arguments declared with keywords for readability
+# (keywords are not necessary as the arguments are positional, not keyword arguments)
+play_game(player_score=0, computer_score=0)
